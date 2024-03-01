4fb72c076813cf5c909ee27b5d24756725e47db2
giotentori <giorgiotentori2@gmail.com>
2024-03-01 15:07:10 +0100

    Update NotebookConfig.java

--- pac-backend/src/main/java/com/example/pacbackend/Notebook/NotebookConfig.java
+++ pac-backend/src/main/java/com/example/pacbackend/Notebook/NotebookConfig.java
@@ -32,30 +32,30 @@ public class NotebookConfig {
                             "ASUS", 8, 1000,Disk.HDD, "Intel I7", "NVIDIA", "Asus usato pochissimo", 900);
         
             
-            /*Immagine 1 Macbook */
-             Image macbookM1Image = new Image();      //Uso costruttore vuoto
-             macbookM1Image.setFileName("MacbookM1-image.png");
-             macbookM1Image.setMimeType("image/png");
-             macbookM1Image.setData(Files.readAllBytes(Paths.get("/Users/davidegamba/Documents/GitHub/Pogetto_Pac-Zanenga_Tentori_Gamba/pac-backend/images-notebook/MacbookM1-image.png")));
-             macbookM1Image.setUriImage("http://localhost:8080/images/MacbookM1-image.png");
+            // /*Immagine 1 Macbook */
+            //  Image macbookM1Image = new Image();      //Uso costruttore vuoto
+            //  macbookM1Image.setFileName("MacbookM1-image.png");
+            //  macbookM1Image.setMimeType("image/png");
+            //  macbookM1Image.setData(Files.readAllBytes(Paths.get("/Users/davidegamba/Documents/GitHub/Pogetto_Pac-Zanenga_Tentori_Gamba/pac-backend/images-notebook/MacbookM1-image.png")));
+            //  macbookM1Image.setUriImage("http://localhost:8080/images/MacbookM1-image.png");
 
-            /*Immagine Lenovo */
-             Image lenovo = new Image();
-             lenovo.setFileName("lenovo1.png");
-             lenovo.setMimeType("image/png");
-             lenovo.setData(Files.readAllBytes(Paths.get("/Users/davidegamba/Documents/GitHub/Pogetto_Pac-Zanenga_Tentori_Gamba/pac-backend/images-notebook/lenovo.png")));
-             lenovo.setUriImage("http://localhost:8080/images/lenovo1.png");
+            // /*Immagine Lenovo */
+            //  Image lenovo = new Image();
+            //  lenovo.setFileName("lenovo1.png");
+            //  lenovo.setMimeType("image/png");
+            //  lenovo.setData(Files.readAllBytes(Paths.get("/Users/davidegamba/Documents/GitHub/Pogetto_Pac-Zanenga_Tentori_Gamba/pac-backend/images-notebook/lenovo.png")));
+            //  lenovo.setUriImage("http://localhost:8080/images/lenovo1.png");
 
-            /*Immagine HP */
-             Image asus = new Image();
-             asus.setFileName("asus.png");
-             asus.setMimeType("image/png");
-             asus.setData(Files.readAllBytes(Paths.get("/Users/davidegamba/Documents/GitHub/Pogetto_Pac-Zanenga_Tentori_Gamba/pac-backend/images-notebook/asus.png")));
-             asus.setUriImage("http://localhost:8080/images/asus.png");
+            // /*Immagine HP */
+            //  Image asus = new Image();
+            //  asus.setFileName("asus.png");
+            //  asus.setMimeType("image/png");
+            //  asus.setData(Files.readAllBytes(Paths.get("/Users/davidegamba/Documents/GitHub/Pogetto_Pac-Zanenga_Tentori_Gamba/pac-backend/images-notebook/asus.png")));
+            //  asus.setUriImage("http://localhost:8080/images/asus.png");
 
-             apple1.setImage(macbookM1Image);
-             lenovo1.setImage(lenovo);
-             asus1.setImage(asus);
+            //  apple1.setImage(macbookM1Image);
+            //  lenovo1.setImage(lenovo);
+            //  asus1.setImage(asus);
 
 
 
